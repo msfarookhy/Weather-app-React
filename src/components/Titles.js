566ef98a30054daf13990e022c64ec54d39fae33
msfarookhy <msfarookhy@gmail.com>
2018-12-29 13:41:19 +0530

    Weather app Started Today

---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Titles extends Component {
+    state = {}
+    render() {
+        return ( 
+            <div>
+            </div>
+          );
+    }
+}
+
+export default Titles;
